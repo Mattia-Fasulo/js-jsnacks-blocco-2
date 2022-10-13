d354c902b7082638235cc76091c04227a40f9afe
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-10-13 15:53:48 +0200

    add index and snack_3

--- snack_2/snack_2.js
+++ snack_2/snack_2.js
@@ -4,28 +4,65 @@ const buttonGenera = document.getElementById('button-genera');
 
 let container = document.getElementById('my-container');
 
+// const generateTable = function () {
+//     container.innerHTML = "";
+//     const tbl = document.createElement("table");
+//     const tblBody = document.createElement("tbody");
+
+//     for (let i = 0; i < 8; i++) {
+        
+//         const row = document.createElement("tr");
+      
+//         for (let j = 0; j < 1; j++) {
+//             const cell = document.createElement("td");
+//             const cellText = document.createTextNode(invitedNames[Math.floor(Math.random() * 7)]);
+//             cell.appendChild(cellText);
+//             row.appendChild(cell);
+//         }
+//         for (let x = 0; x < 1; x++) {
+//             const cell = document.createElement("td");
+//             const cellText = document.createTextNode(invitedSurname[Math.floor(Math.random() * 7)]);
+//             cell.appendChild(cellText);
+//             row.appendChild(cell);
+//         }
+//         tblBody.appendChild(row);
+//     }
+
+//     tbl.appendChild(tblBody);
+//     container.appendChild(tbl);
+
+
+
+
+// }
+
 const generateTable = function () {
     container.innerHTML = "";
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
-
-    for (let i = 0; i < 8; i++) {
+    let i = 0
+    while ( i < 8) {
         
         const row = document.createElement("tr");
-      
-        for (let j = 0; j < 1; j++) {
+        let j = 0;
+        while (j < 1) {
             const cell = document.createElement("td");
             const cellText = document.createTextNode(invitedNames[Math.floor(Math.random() * 7)]);
             cell.appendChild(cellText);
             row.appendChild(cell);
+            j++;
         }
-        for (let x = 0; x < 1; x++) {
+
+        let x = 0
+        while (x < 1) {
             const cell = document.createElement("td");
             const cellText = document.createTextNode(invitedSurname[Math.floor(Math.random() * 7)]);
             cell.appendChild(cellText);
             row.appendChild(cell);
+            x++;
         }
         tblBody.appendChild(row);
+        i++;
     }
 
     tbl.appendChild(tblBody);
@@ -35,6 +72,8 @@ const generateTable = function () {
 
 
 }
+
+
 
 buttonGenera.addEventListener('click', generateTable);
 
---
+++ snack_3/snack_3.js
@@ -0,0 +1,36 @@
+const arrNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const contArr = document.getElementById('cont-arr');
+const contRes = document.getElementById('result');
+const button = document.getElementById('btn-somma');
+console.log(arrNum)
+
+for( i = 0; i < arrNum.length; i++){
+    let boxArr = document.createElement('div');
+    boxArr.className = ("col-1 card");
+    boxArr.innerHTML = `${arrNum[i]}`
+    if(!(i % 2) == 0){
+        boxArr.className = ("col-1 card elem-disp")
+    }
+    contArr.append(boxArr);
+       
+}
+
+let calc = function(){
+    contRes.innerHTML = "";
+    let messaggio = document.createElement('p');
+    let risultato = document.createElement('p');
+    let somma = 0;
+
+    for(x = 0; x < arrNum.length; x++){
+        if(!(x % 2) == 0){
+            somma += parseInt(arrNum[x]);
+        }
+    }
+    messaggio.innerHTML = "La somma dei numeri bianchi è:";
+    risultato.innerHTML = somma;
+    contRes.append(messaggio);
+    contRes.append(risultato);
+
+}
+
+button.addEventListener('click', calc);
