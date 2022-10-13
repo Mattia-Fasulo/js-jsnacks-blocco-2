30eb0f20440779ccf327d17379000361616b961a
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-10-13 10:20:51 +0200

    remove btn refresh

--- snack_2/snack_2.js
+++ snack_2/snack_2.js
@@ -1,26 +1,27 @@
 const invitedNames = ['Mattia', 'Vittorio', 'Chiara', 'Susanna', 'Antonio', 'Vincenzo', 'Eugenia', 'Matilde'];
 const invitedSurname = ['Stark', 'Lannister', 'Targaryen', 'Baratheon', 'Tyrell', 'Greyjoy', 'Arryn', 'Martell'];
 const buttonGenera = document.getElementById('button-genera');
-const buttonCanc = document.getElementById('button-canc');
+
 let container = document.getElementById('my-container');
 
 const generateTable = function () {
-
+    container.innerHTML = "";
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
 
     for (let i = 0; i < 8; i++) {
+        
         const row = document.createElement("tr");
-
+      
         for (let j = 0; j < 1; j++) {
             const cell = document.createElement("td");
-            const cellText = document.createTextNode(invitedNames[Math.floor(Math.random() * (7 - 0) + 0)]);
+            const cellText = document.createTextNode(invitedNames[Math.floor(Math.random() * 7)]);
             cell.appendChild(cellText);
             row.appendChild(cell);
         }
         for (let x = 0; x < 1; x++) {
             const cell = document.createElement("td");
-            const cellText = document.createTextNode(invitedSurname[Math.floor(Math.random() * (7 - 0) + 0)]);
+            const cellText = document.createTextNode(invitedSurname[Math.floor(Math.random() * 7)]);
             cell.appendChild(cellText);
             row.appendChild(cell);
         }
@@ -35,11 +36,6 @@ const generateTable = function () {
 
 }
 
-const refresh = function () {
-    window.location.reload();
-}
-
-buttonCanc.addEventListener('click', refresh);
 buttonGenera.addEventListener('click', generateTable);
 
 
